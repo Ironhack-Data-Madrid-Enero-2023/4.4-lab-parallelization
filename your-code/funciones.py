f24cbfacd880c0bf418ee30ae0df5492f5e20bbf
Pedro Jodar <pedrojodargabarron@gmail.com>
2023-02-09 17:48:46 +0100

    lab resuelto

---
+++ your-code/funciones.py
@@ -0,0 +1,11 @@
+def index_page(link):
+    try:
+        html = requests.get(link).text
+        nombre_arc = slugify(link) +'.html'
+        file = open(nombre_arc, 'w')
+        file.write(html)
+        file.close()
+ 
+    except:
+        pass
+    
